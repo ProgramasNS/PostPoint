34a93dcff44e4eb0c3e6c524d7391a954cdb7945
ProgramasNS <programasns@gmail.com>
2026-06-28 22:31:53 -0300

    Corrigindo erros com o banco de dados

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,13 +3,15 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const dbUrl = process.env["DATABASE_URL"] ?? ""
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: dbUrl,
   },
 });
 
