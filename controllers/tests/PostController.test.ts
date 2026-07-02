96af17574bbf07291dfc0dbf2e85b8a0d82f823e
ProgramasNS <programasns@gmail.com>
2026-07-02 20:31:54 -0300

    Adicionando o teste do PostController

---
+++ controllers/tests/PostController.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test} from "@jest/globals";
+import request from 'supertest';
+import app from "../../app";
+import HttpCodes from "../../objects/Http";
+
+//Função correspondente à função "criarPost"
+describe(
+    'POST /new', () => {
+        test(
+            'Criando novo post', async () => {
+                const userId = 1;
+                const content = {
+                    title: "Título para teste", content: "Conteúdo para teste", date: new Date(), user_id: userId
+                };
+                const res = await request(app).post('/new').send(content);
+                expect(res.body.content).toBe(content);
+                expect(res.statusCode).toBe(HttpCodes.CREATED);
+            }
+        )
+    }
+);
+
+//
