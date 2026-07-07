b1625b6402057b631989dda7637472ec113150b1
ProgramasNS <programasns@gmail.com>
2026-07-07 18:49:47 -0300

    Removendo migrations desnecessárias

--- prisma/migrations/20260626172722_ajuste_relacoes/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "comments" DROP CONSTRAINT "fkh4c7lvsc298whoyd4w9ta25cr";
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "fkh4c7lvsc298whoyd4w9ta25cr" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
