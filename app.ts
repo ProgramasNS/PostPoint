abac31273b7678fc729916548bcc9c49f1f46246
ProgramasNS <programasns@gmail.com>
2026-07-07 18:54:37 -0300

    Adicionando o body-parser

--- app.ts
+++ app.ts
@@ -3,10 +3,13 @@ import cors from 'cors'
 import userRouter from './routes/UserRoutes'
 import postRoute from './routes/PostRoutes'
 import commentRoute from './routes/CommentRoutes';
+import bodyParser from 'body-parser'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/user', userRouter);
 app.use('/api/post', postRoute);
 app.use('/api/post/comment', commentRoute);
