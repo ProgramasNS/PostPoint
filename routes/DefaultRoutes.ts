c0d997da3308aab9d8c77ccd17eb3e2ea4071c45
ProgramasNS <programasns@gmail.com>
2026-08-03 18:47:20 -0300

    Adding the default route

--- routes/DefaultRoutes.ts
+++ routes/DefaultRoutes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response} from "express";
 import HttpCodes from "../objects/Http";
 
 const defaultRoute = Router();
-defaultRoute.get('/', (req: Request, res: Response) => {
+defaultRoute.get('', (req: Request, res: Response) => {
     return res.status(HttpCodes.OK).json({message: "Bem-vindo(a) ao PostPoint!"});
 })
 
